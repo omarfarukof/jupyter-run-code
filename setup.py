cdbedfcd11c76569197447fc93a3ff4ec3889960
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2023-06-06 14:08:32 +0600

    l

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jupyter-run-code',
+    name='jupyter_run_code',
     version='0.1',
     packages=find_packages(),
     install_requires=[
